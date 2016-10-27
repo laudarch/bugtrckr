e68ac9d5ae8719111fd6ca2c94dd78ed19603caf
laudarch <laudarch@qremiaevolution.org>
2016-10-27 18:37:53 +0000

    Small Customizations

--- gui/home.tpl.php
+++ gui/home.tpl.php
@@ -1,27 +1,8 @@
 <div class="home">
     <h2>Bugtrckr</h2>
 
-    <p>Bugtrckr is a minimal but feature rich web application to manage projects. It's written in pure PHP and uses <a href="http://fatfree.sf.net">Fat-Free Framework</a> to make <strike>our life</strike> coding this project easier.</p>
-    <p>Bugtrckr is open source and is released under the terms of <a href="http://www.gnu.org/licenses/lgpl.txt">LGPL</a></p>
-
-    <h3>Features</h3>
-    <ul>
-        <li>Role based ACL</li>
-        <li>Multi projects</li>
-        <li>Issue tracking</li>
-        <li>Milestones</li>
-        <li>User management</li>
-        <li>Localization</li>
-        <li><em>Further features will follow ...</em></li>
-    </ul>
+    <p>Private Bug tracker.</p>
 
     <h3>Help & Support</h3>
-    <p>Bugtrckr is still under developement, so errors may appear or functions may not work, but feel free to file issues or feature requests on our <a href="http://github.com/sn0opy/bugtrckr">Github page</a> or join our chat on irc.kassel.ccc.de channel #cccks (you will auto-join it).</p>
-
-    <h3>Bugtrckr-Team</h3>
-    <p>Our current team consists of two persons</p>
-    <ul>
-        <li>Sascha Ohms</li>
-        <li>Philipp Hirsch</li>
-    </ul>
+    <p><a href='mailto:dev@mobile2i.com'>dev@mobile2i.com</a></p>
 </div>
